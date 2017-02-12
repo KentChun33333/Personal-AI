78f8919db199631bd4f5199af971e29a3dacdc0d
KentChun33333 <kentchun33333@gmail.com>
2017-02-12 14:23:59 +0800

    up

--- get_stock_data.py
+++ get_stock_data.py
@@ -2,13 +2,36 @@ from yahoo_finance import Share
 import pandas as pd
 import datetime
 
+
+STOCK_ref  =  'tw_stock_data/all_stock_201701.txt'
+OPTION_ref = 'tw_stock_data/all_stock_options_201702.txt'
+
 def getStock(id):
     stock = Share(str(id)+'.TW')
     today = datetime.date.today()
     data = stock.get_historical('2016-12-01', str(today))
     return data
 
+def get_id(address):
+    with open(address, 'r') as f : 
+        for line in f:
+            id = line.split(' ')[0]
+            try: 
+                assert len(id)== 4 or 6
+                yield id
+            except Exception as e :
+                print e 
+    
+
+
 
 if __name__=='__main__':
-    data =  getStock(1517)
-    df = pd.DataFrame(data)
+    # test :: pass
+    # data =  getStock(1517)
+    # df = pd.DataFrame(data)
+
+    # test :: pass
+    for i in get_id(STOCK_ref):
+        print i
+    for i in get_id(OPTION_ref):
+        print i
